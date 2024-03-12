60d66a55145d8f71e2ccd9fa08fe20046ab5eff0
lukas.belan <lukas.belan@globallogic.com>
2024-03-13 00:58:05 +0100

    COSA: finished COSA_Init implementation
    Still contains maybe redundant code (check TODO comments)

--- source/dml/openvpn_client_ssp/plugin_main.c
+++ source/dml/openvpn_client_ssp/plugin_main.c
@@ -44,6 +44,35 @@
 
 #define THIS_PLUGIN_VERSION                         1
 
+extern COSAGetParamValueByPathNameProc    g_GetParamValueByPathNameProc;
+extern COSASetParamValueByPathNameProc    g_SetParamValueByPathNameProc;
+extern COSAGetParamValueStringProc        g_GetParamValueString;
+extern COSAGetParamValueUlongProc         g_GetParamValueUlong;
+extern COSAGetParamValueIntProc           g_GetParamValueInt;
+extern COSAGetParamValueBoolProc          g_GetParamValueBool;
+extern COSASetParamValueStringProc        g_SetParamValueString;
+extern COSASetParamValueUlongProc         g_SetParamValueUlong;
+extern COSASetParamValueIntProc           g_SetParamValueInt;
+extern COSASetParamValueBoolProc          g_SetParamValueBool;
+extern COSAGetInstanceNumbersProc         g_GetInstanceNumbers;
+extern 
+extern COSAValidateHierarchyInterfaceProc g_ValidateInterface;           // TODO: Check if these needed
+extern COSAGetHandleProc                  g_GetRegistryRootFolder;       // TODO: Check if these needed
+extern COSAGetInstanceNumberByIndexProc   g_GetInstanceNumberByIndex;    // TODO: Check if these needed
+extern COSAGetInterfaceByNameProc         g_GetInterfaceByName;          // TODO: Check if these needed
+extern COSAGetHandleProc                  g_GetMessageBusHandle;         // TODO: Check if these needed
+extern COSAGetSubsystemPrefixProc         g_GetSubsystemPrefix;          // TODO: Check if these needed
+extern PCCSP_CCD_INTERFACE                g_pPnmCcdIf;                   // TODO: Check if these needed
+extern ANSC_HANDLE                        g_MessageBusHandle;            // TODO: Check if these needed
+extern char*                              g_SubsystemPrefix;
+extern COSARegisterCallBackAfterInitDmlProc  g_RegisterCallBackAfterInitDml;
+extern COSARepopulateTableProc            g_COSARepopulateTable;
+
+//void *                       g_pDslhDmlAgent;
+extern ANSC_HANDLE     g_MessageBusHandle_Irep;
+extern char            g_SubSysPrefix_Irep[32];
+PCOSA_BACKEND_MANAGER_OBJECT g_pCosaBEManager;
+
 int ANSC_EXPORT_API
 COSA_Init
     (
@@ -52,6 +81,25 @@ COSA_Init
     )
 {
     PCOSA_PLUGIN_INFO               pPlugInfo  = (PCOSA_PLUGIN_INFO)hCosaPlugInfo;
+
+    COSAGetParamValueByPathNameProc pGetParamValueByPathNameProc = (COSAGetParamValueByPathNameProc)NULL;
+    COSASetParamValueByPathNameProc pSetParamValueByPathNameProc = (COSASetParamValueByPathNameProc)NULL;
+    COSAGetParamValueStringProc     pGetStringProc              = (COSAGetParamValueStringProc       )NULL;
+    COSAGetParamValueUlongProc      pGetParamValueUlongProc     = (COSAGetParamValueUlongProc        )NULL;
+    COSAGetParamValueIntProc        pGetParamValueIntProc       = (COSAGetParamValueIntProc          )NULL;
+    COSAGetParamValueBoolProc       pGetParamValueBoolProc      = (COSAGetParamValueBoolProc         )NULL;
+    COSASetParamValueStringProc     pSetStringProc              = (COSASetParamValueStringProc       )NULL;
+    COSASetParamValueUlongProc      pSetParamValueUlongProc     = (COSASetParamValueUlongProc        )NULL;
+    COSASetParamValueIntProc        pSetParamValueIntProc       = (COSASetParamValueIntProc          )NULL;
+    COSASetParamValueBoolProc       pSetParamValueBoolProc      = (COSASetParamValueBoolProc         )NULL;
+    COSAGetInstanceNumbersProc      pGetInstanceNumbersProc     = (COSAGetInstanceNumbersProc        )NULL;
+
+    COSAValidateHierarchyInterfaceProc
+        pValInterfaceProc           = (COSAValidateHierarchyInterfaceProc)NULL;
+    COSAGetHandleProc               pGetRegistryRootFolder      = (COSAGetHandleProc                 )NULL;
+    COSAGetInstanceNumberByIndexProc
+        pGetInsNumberByIndexProc    = (COSAGetInstanceNumberByIndexProc  )NULL;
+    COSAGetInterfaceByNameProc      pGetInterfaceByNameProc     = (COSAGetInterfaceByNameProc        )NULL;
 
     if ( uMaxVersionSupported < THIS_PLUGIN_VERSION )
     {
@@ -76,6 +124,191 @@ COSA_Init
     pPlugInfo->RegisterFunction(pPlugInfo->hContext, "Tunnel_Synchronize",  Tunnel_Synchronize);
     pPlugInfo->RegisterFunction(pPlugInfo->hContext, "Tunnel_GetParamStringValue",  Tunnel_GetParamStringValue);
     pPlugInfo->RegisterFunction(pPlugInfo->hContext, "Tunnel_SetParamStringValue",  Tunnel_SetParamStringValue);
+
+
+
+    pGetParamValueByPathNameProc = (COSAGetParamValueByPathNameProc)pPlugInfo->AcquireFunction("COSAGetParamValueByPathName");
+    if( pGetParamValueByPathNameProc != NULL)
+    {
+        g_GetParamValueByPathNameProc = pGetParamValueByPathNameProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+    pSetParamValueByPathNameProc = (COSASetParamValueByPathNameProc)pPlugInfo->AcquireFunction("COSASetParamValueByPathName");
+
+    if( pSetParamValueByPathNameProc != NULL)
+    {
+        g_SetParamValueByPathNameProc = pSetParamValueByPathNameProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+    pGetStringProc = (COSAGetParamValueStringProc)pPlugInfo->AcquireFunction("COSAGetParamValueString");
+
+    if( pGetStringProc != NULL)
+    {
+        g_GetParamValueString = pGetStringProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+    pGetParamValueUlongProc = (COSAGetParamValueUlongProc)pPlugInfo->AcquireFunction("COSAGetParamValueUlong");
+
+    if( pGetParamValueUlongProc != NULL)
+    {
+        g_GetParamValueUlong = pGetParamValueUlongProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+
+    pGetParamValueIntProc = (COSAGetParamValueIntProc)pPlugInfo->AcquireFunction("COSAGetParamValueInt");
+
+    if( pGetParamValueIntProc != NULL)
+    {
+        g_GetParamValueInt = pGetParamValueIntProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+    pGetParamValueBoolProc = (COSAGetParamValueBoolProc)pPlugInfo->AcquireFunction("COSAGetParamValueBool");
+
+    if( pGetParamValueBoolProc != NULL)
+    {
+        g_GetParamValueBool = pGetParamValueBoolProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+    pSetStringProc = (COSASetParamValueStringProc)pPlugInfo->AcquireFunction("COSASetParamValueString");
+
+    if( pSetStringProc != NULL)
+    {
+        g_SetParamValueString = pSetStringProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+    pSetParamValueUlongProc = (COSASetParamValueUlongProc)pPlugInfo->AcquireFunction("COSASetParamValueUlong");
+
+    if( pSetParamValueUlongProc != NULL)
+    {
+        g_SetParamValueUlong = pSetParamValueUlongProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+
+    pSetParamValueIntProc = (COSASetParamValueIntProc)pPlugInfo->AcquireFunction("COSASetParamValueInt");
+
+    if( pSetParamValueIntProc != NULL)
+    {
+        g_SetParamValueInt = pSetParamValueIntProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+    pSetParamValueBoolProc = (COSASetParamValueBoolProc)pPlugInfo->AcquireFunction("COSASetParamValueBool");
+
+    if( pSetParamValueBoolProc != NULL)
+    {
+        g_SetParamValueBool = pSetParamValueBoolProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+    pGetInstanceNumbersProc = (COSAGetInstanceNumbersProc)pPlugInfo->AcquireFunction("COSAGetInstanceNumbers");
+
+    if( pGetInstanceNumbersProc != NULL)
+    {
+        g_GetInstanceNumbers = pGetInstanceNumbersProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+    pValInterfaceProc = (COSAValidateHierarchyInterfaceProc)pPlugInfo->AcquireFunction("COSAValidateHierarchyInterface");
+
+    if ( pValInterfaceProc )
+    {
+        g_ValidateInterface = pValInterfaceProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+    pGetRegistryRootFolder = (COSAGetHandleProc)pPlugInfo->AcquireFunction("COSAGetRegistryRootFolder");
+
+    if ( pGetRegistryRootFolder != NULL )
+    {
+        g_GetRegistryRootFolder = pGetRegistryRootFolder;
+    }
+    else
+    {
+        goto EXIT;
+    }
+    pGetInsNumberByIndexProc = (COSAGetInstanceNumberByIndexProc)pPlugInfo->AcquireFunction("COSAGetInstanceNumberByIndex");
+
+    if ( pGetInsNumberByIndexProc != NULL )
+    {
+        g_GetInstanceNumberByIndex = pGetInsNumberByIndexProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+    pGetInterfaceByNameProc = (COSAGetInterfaceByNameProc)pPlugInfo->AcquireFunction("COSAGetInterfaceByName");
+
+    if ( pGetInterfaceByNameProc != NULL )
+    {
+        g_GetInterfaceByName = pGetInterfaceByNameProc;
+    }
+    else
+    {
+        goto EXIT;
+    }
+    g_pPnmCcdIf = g_GetInterfaceByName(g_pDslhDmlAgent, CCSP_CCD_INTERFACE_NAME);
+
+    if ( !g_pPnmCcdIf )
+    {
+        goto EXIT;
+    }
+    g_RegisterCallBackAfterInitDml = (COSARegisterCallBackAfterInitDmlProc)pPlugInfo->AcquireFunction("COSARegisterCallBackAfterInitDml");
+
+    if ( !g_RegisterCallBackAfterInitDml )
+    {
+        goto EXIT;
+    }
+    g_COSARepopulateTable = (COSARepopulateTableProc)pPlugInfo->AcquireFunction("COSARepopulateTable");
+
+    if ( !g_COSARepopulateTable )
+    {
+        goto EXIT;
+    }
+    /* Get Message Bus Handle */
+    g_GetMessageBusHandle = (COSAGetHandleProc)pPlugInfo->AcquireFunction("COSAGetMessageBusHandle");
+    if ( g_GetMessageBusHandle == NULL )
+    {
+        goto EXIT;
+    }
+    g_MessageBusHandle = (ANSC_HANDLE)g_GetMessageBusHandle(g_pDslhDmlAgent);
+    if ( g_MessageBusHandle == NULL )
+    {
+        goto EXIT;
+    }
+
+
     return  0;
 }
 
